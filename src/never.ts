0a8cb78a73a147ef47f6c2a543c9d26f084951ea
Eugene-301 <ablepiha@duck.com>
2025-07-07 20:05:41 +0300

    Added new example to never.ts

--- src/never.ts
+++ src/never.ts
@@ -8,3 +8,23 @@ let stringValue: string = neverValue;
 // Cause error:
 // let stringValue: string = '123';
 // let neverValue: never = srtrinValue
+
+enum Values {
+  FIRST,
+  SECOND,
+}
+
+function func(value: Values) {
+  switch (value) {
+    case Values.FIRST:
+      return value;
+      break;
+    case Values.SECOND:
+      return value;
+      break;
+    default:
+      const check: never = value;
+      return value;
+      break;
+  }
+}
