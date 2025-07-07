f4fe11fbaf3c1181ae32fa016781d9b290d7a7c8
Eugene-301 <ablepiha@duck.com>
2025-07-07 18:30:29 +0300

    Union and intersecion in progress

---
+++ src/unionAndIntersection.ts
@@ -0,0 +1,24 @@
+// union type
+// is means that variable can have value type one of specified data types
+
+let data: number | string;
+data = 5;
+data = "string";
+
+let color: "red" | "green";
+
+// unioin type in objects
+type MainInfo = {
+    firstName: string;
+    lastName: string;
+}
+
+type AdditionalInfo = {
+    age: number;
+}
+
+type FullInfo = AdditionalInfo | MainInfo;
+
+const info0: FullInfo = { firstName: "123", lastName: "123", age: 123 }
+const info1: FullInfo = { firstName: "qwe", lastName: "qwe" }
+const info2: FullInfo = { age: 345 }
